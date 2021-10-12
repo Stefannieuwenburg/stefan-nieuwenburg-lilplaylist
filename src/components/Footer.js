a4c9dbed0fbe846f5bac56d4732728789f98dcb7
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-10-12 18:48:46 +0200

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function Footer () {
+    return (
+        <h2 style={{textAlign: 'center', fontSize: "2em"}}>Stefan Nieuwenburg</h2>
+    )
+}
+
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function Header () {
+    return (
+        <h1 style={{textAlign: 'center', fontSize: "3em"}}>Winc Lil-Playlist</h1>
+    )
+}
+
+
+export default Header;
